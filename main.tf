08712a31b580401d09beb8d4a05bc1567050afab
BogdanEvstygneiv <bonhead@ukr.net>
2025-03-16 16:26:12 +0100

    Add user command

--- main.tf
+++ main.tf
@@ -7,6 +7,11 @@ resource "aws_instance" "app" {
 	instance_type = "t3.micro"
 	availability_zone = "eu-north-1a"
 	ami = "ami-09a9858973b288bdd"
+	user_data = <<-EOF
+		#!/bin/bash
+		echo "Hello, World" > index.html
+		nohup busybox httpd -f -p 8080 &
+		EOF
 	tags = {
 	Name = "terraform-example"
 }
